6dcff7015b500adc4630c6debbb870aa50544e67
Nik-nicole <paezvasqueznicole18@gmail.com>
2026-03-17 14:08:28 -0500

    fix : add in next a funtion to can load the videos

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*.(mp4|webm|ogg)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'video/mp4',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
